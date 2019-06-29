f736916c46e5189cdae5161fba5ffd33bf6b6c7b
theduodecim <juansebastian.012@gmail.com>
2019-06-29 20:37:14 -0300

    Adding Steeps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,23 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
     currentUser: any;
-
+    steeps: Array<any> = [
+        { steep: 'Create Base Project Structure & Cli Config.' },
+        { steep: 'Creating fake backend global api and authentification Service that will handle the bancked.' },
+        { steep: 'Authentification Login Component creation Logic and ViewTemplate.' },
+        { steep: 'Injecting authentification to the AppComponent to check the logedCurrentUser.' },
+        { steep: 'Guards creating and adding to routingModule.' },
+        { steep: 'Interceptors & Error Interceptors for the HTTP Request.' },
+        { steep: 'Updating the fake backend for Register Users Storing each user in the localStorage.' },
+        { steep: 'Creating UserService that manage the Crud of the users.' },
+        { steep: 'Logic that handles formGroup validation, Submission, Error Handling , and binding to the Template View.' },
+        { steep: 'Adding Success Message verifying the sended QueryParams Property Option.' },
+        { steep: 'Adding Crud functionalities to the Fake-Backend.' },
+        { steep: 'Creating the Logic for the Home Component and the Template to display eachuser registered and delete.' },
+        { steep: 'Alert Service, 14 Creating the Alert Component and adding the selector to the app-component.' },
+        { steep: 'Refactoring Login Component with the new Alert Service' },
+        { steep: 'Refactoring Register Component with the new AlertService' }
+    ];
     constructor(private authService: AuthenticationService, private router: Router, ) {
         this.authService.currentUser.subscribe(x => this.currentUser = x);
     }
